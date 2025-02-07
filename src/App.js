e5c7aadfb98c5f09c317f27f6cc1fecc6f9c82b0
Pavapa <pavapa01@thu.de>
2025-02-07 18:29:02 +0100

    comented out login page to see homepage

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
 import React, { useEffect } from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
-//import { StandardSignIn as Login } from './components/Login';
-import  { StandardSignIn as Login } from './components/Login';
 import Frame1 from './container/Home';
-
-
+//import { StandardSignIn as Login } from './components/Login';
 
 const App = () => {
- const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  useEffect(() => {
-    const User = localStorage.getItem('user');
-    const parsedUser = User && User !== 'undefined' ? JSON.parse(User) : null;
+    useEffect(() => {
+        const User = localStorage.getItem('user');
+        const parsedUser = User && User !== 'undefined' ? JSON.parse(User) : null;
 
-if (!parsedUser) {
-  navigate('/login');
-}
-  }, [navigate]);
+        //to see the LOGIN page, remove the // from line below and add <Route path="login" element={<Login />} /> in the Routes section below
+        // if (!parsedUser) navigate('/login');
+    }, [navigate]);
 
-  return (
-    <Routes>
-      <Route path="login" element={<Login />} />
-      <Route path="/*" element={<Frame1 />} />
-    </Routes>
-  );
+    return (
+        <Routes>
+
+            <Route path="/*" element={<Frame1 />} />
+        </Routes>
+    );
 };
 
 export default App;
